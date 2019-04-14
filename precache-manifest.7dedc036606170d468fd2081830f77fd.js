d2e8ee84f741ec48c0f82c7b63c75e1ac2e3c4bc
Maxim Andreev <m.andreev96@mail.ru>
2019-04-14 17:44:14 +0300

    added app

---
+++ precache-manifest.7dedc036606170d468fd2081830f77fd.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "5a798cdadc7cd321e3f72425b70bface",
+    "url": "/static/media/OpenSansRegular.5a798cda.ttf"
+  },
+  {
+    "revision": "96207c468d886f3a2b30",
+    "url": "/static/css/main.179e6aa3.chunk.css"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "7892c8fa495bb2fe8d97",
+    "url": "/static/js/2.858e46d7.chunk.js"
+  },
+  {
+    "revision": "775c3814e9c1f228d495333e07580d59",
+    "url": "/static/media/AndyBold.775c3814.ttf"
+  },
+  {
+    "revision": "f51d593e2ab59a38dd41cc76a3f88460",
+    "url": "/static/media/OpenSansLight.f51d593e.ttf"
+  },
+  {
+    "revision": "96207c468d886f3a2b30",
+    "url": "/static/js/main.d1cafffe.chunk.js"
+  },
+  {
+    "revision": "0062c34665a3fc0f2278cd4e955702ec",
+    "url": "/static/media/OpenSansBold.0062c346.ttf"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "7892c8fa495bb2fe8d97",
+    "url": "/static/css/2.9e9378f1.chunk.css"
+  },
+  {
+    "revision": "55e9e2d53daa7b00c2a66ec7fccf78d6",
+    "url": "/index.html"
+  }
+];
